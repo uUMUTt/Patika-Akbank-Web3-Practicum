6011798cbee3f1df06ca2448f7bf721b3fc8e209
Umut Karadas <81423078+uUMUTt@users.noreply.github.com>
2022-10-02 02:35:48 +0300

    Add files via upload

---
+++ TodoList.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+
+contract TodoList {
+
+    struct Todo {
+        string text;
+        bool completed;
+    }
+
+    Todo[] public todos;
+
+    function create(string calldata _text) external {
+        todos.push(Todo({
+            text : _text,
+            completed : false
+        }));            
+    }
+
+    function updateText(uint _index, string calldata _text) external {
+        todos[_index].text = _text;
+    }
+
+    function get(uint _index) external view returns(string memory , bool) {
+        Todo memory todo = todos[_index];
+        return (todo.text, todo.completed);                
+    }
+
+    function togleCompleted(uint _index) external {
+        todos[_index].completed = !todos[_index].completed;
+    }
+}
